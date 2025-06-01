c258ef207630d73c3d284aa16f41d26aa0f0bc89
Paul <115771869+Polypheus@users.noreply.github.com>
2025-06-01 21:06:37 +0000

    interactive

--- components/about.vue
+++ components/about.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="container mx-auto flex flex-col items-center justify-center h-screen">
+  <div class="container mx-auto flex flex-col items-center justify-center h-screen block">
     <div class="about-text" ref="aboutText">
       <h1>
-        I’m Paul Andrew Consunji, a passionate and determined IT graduate from Rizal Technological University.
+        I’m <span class="hover-about">Paul Andrew Consunji</span>, a passionate and determined IT graduate from <span class="hover-about">Rizal Technological University</span>.
         <br /><br />
-        I specialize in front-end development, blending clean UI with smooth user experiences. I’ve worked with tools like Vue/Nuxt, Tailwind CSS, and GSAP to bring interfaces to life.
+        I specialize in <span class="hover-about">front-end development</span>, blending clean UI with <span class="hover-about">smooth user experiences</span>. I’ve worked with tools like <span class="hover-about">Vue/Nuxt</span>, <span class="hover-about">Tailwind CSS</span>, and <span class="hover-about">GSAP</span> to bring interfaces to life.
         <br /><br />
-        My journey started with curiosity—fixing computers, designing graphics, and building web apps that solve real problems. Whether it's managing data, enhancing workflows, or designing interactive effects, I aim to create digital solutions that are useful, fast, and simple.
+        My journey started with <span class="hover-about">curiosity—fixing computers</span>, <span class="hover-about">designing graphics</span>, and <span class="hover-about">building web apps</span> that solve real problems. Whether it's <span class="hover-about">managing data</span>, <span class="hover-about">enhancing workflows</span>, or <span class="hover-about">designing interactive effects</span>, I aim to create digital solutions that are <span class="hover-about">useful</span>, <span class="hover-about">fast</span>, and <span class="hover-about">simple</span>.
         <br /><br />
-        I’m based in Metro Manila, Philippines—and always open to new challenges and opportunities to grow as a developer.
+        I’m based in <span class="hover-about">Metro Manila</span>,<span class="hover-about">Philippines</span>—and always open to new challenges and opportunities to grow as a developer.
       </h1>
     </div>
   </div>
